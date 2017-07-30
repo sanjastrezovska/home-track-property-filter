3559dcdd3ff60f04017b4ac601aed4706c3f115a
sanjastrezovska <sanja.strezovska@gmail.com>
2017-07-30 12:42:10 +1000

    Update Settings

--- app.js
+++ app.js
@@ -62,6 +62,6 @@ app.post('/properties/completed/htv', function (req, res) {
     }
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(8080, function () {
     console.log("Listening on port %s...", server.address().port);
 });
